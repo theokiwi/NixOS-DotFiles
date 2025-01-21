2315ec81fae958e4272f2929f6717f56ff42cb09
theokiwi <theodinizv@gmail.com>
2025-01-21 11:56:20 -0300

    Init Config

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+description = "Example Flake";
+inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+};
+
+outputs = {self, nixpkgs, home-manager, ...}:
+ let
+        lib = nixpkgs.lib;
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+        in {
+
+    nixosConfigurations = {
+        theokiwi = lib.nixosSystem{
+            inherit system;
+            modules = [ ./configuration.nix];
+            };
+        };
+
+    homeConfigurations = {
+        theokiwi = home-manager.lib.homeManagerConfiguration{
+            inherit pkgs;
+            modules = [ ./home.nix];
+            };
+        };
+    };
+}
